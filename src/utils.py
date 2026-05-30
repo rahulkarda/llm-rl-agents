ee37b6548e0d8ff3e85fee472caf340242ddc0c1
rahulkarda <rahulkarda2002@gmail.com>
2026-05-30 10:20:09 +0000

    add dict_to_str util for pretty-printing nested dicts

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,4 @@
-def flatten_dict(d, parent_key='', sep='.'):
+def flatten_dict(d, parent_key='', sep='.'): 
     """Flatten a nested dictionary, joining keys with sep."""
     items = []
     for k, v in d.items():
@@ -8,3 +8,25 @@ def flatten_dict(d, parent_key='', sep='.'):
         else:
             items.append((new_key, v))
     return dict(items)
+
+
+def dict_to_str(d, indent=0):
+    """
+    Pretty-print a (potentially nested) dict as an indented string for logging/debugging.
+    Args:
+        d: dict to print
+        indent: current indentation level (internal use)
+    Returns:
+        str
+    """
+    lines = []
+    ind = '  ' * indent
+    if not isinstance(d, dict):
+        return repr(d)
+    for k, v in d.items():
+        if isinstance(v, dict):
+            lines.append(f"{ind}{k}:")
+            lines.append(dict_to_str(v, indent=indent+1))
+        else:
+            lines.append(f"{ind}{k}: {repr(v)}")
+    return '\n'.join(lines)
