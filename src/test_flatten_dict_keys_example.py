6dd000c83ecb554b73148a17279a411a947210ad
rahulkarda <rahulkarda2002@gmail.com>
2026-07-18 07:06:58 +0000

    add tiny test for flatten_dict_keys utility

--- src/test_flatten_dict_keys_example.py
+++ src/test_flatten_dict_keys_example.py
@@ -3,6 +3,5 @@ from utils import flatten_dict_keys
 if __name__ == "__main__":
     d = {'a': 1, 'b': {'c': 2, 'd': {'e': 3}}}
     keys = flatten_dict_keys(d)
-    print("Original dict:", d)
-    print("Flattened keys:", keys)
-    # Expected: ['a', 'b.c', 'b.d.e']
+    print("d:", d)
+    print("flatten_dict_keys:", keys)  # Expected: ['a', 'b.c', 'b.d.e']
