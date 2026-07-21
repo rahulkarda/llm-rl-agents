9a53ec4ffc775e05a5dd50f5faf79ed8789d074b
rahulkarda <rahulkarda2002@gmail.com>
2026-07-21 16:11:33 +0000

    Add tiny example for deep_get utility
    Show how to use deep_get for safe nested dict extraction, complementing README and tests.

--- src/test_deep_get_example.py
+++ src/test_deep_get_example.py
@@ -1,25 +1,14 @@
 from utils import deep_get
 
 if __name__ == "__main__":
-    d = {
-        'a': {'b': {'c': 5}},
-        'x': {'y': 42},
-        'empty': {},
-        'none': None,
-    }
-    v1 = deep_get(d, ['a', 'b', 'c'])  # should be 5
-    v2 = deep_get(d, ['x', 'y'])       # should be 42
-    v3 = deep_get(d, ['a', 'b', 'z'])  # should be None
-    v4 = deep_get(d, ['empty', 'foo']) # should be None
-    v5 = deep_get(d, ['none', 'foo'])  # should be None
-    print("deep_get(d, ['a', 'b', 'c']):", v1)
-    print("deep_get(d, ['x', 'y']):", v2)
-    print("deep_get(d, ['a', 'b', 'z']):", v3)
-    print("deep_get(d, ['empty', 'foo']):", v4)
-    print("deep_get(d, ['none', 'foo']):", v5)
-    assert v1 == 5
-    assert v2 == 42
-    assert v3 is None
-    assert v4 is None
-    assert v5 is None
-    print("deep_get tests passed.")
+    d = {'a': {'b': {'c': 5}}, 'x': {'y': 42}}
+    val1 = deep_get(d, ['a', 'b', 'c'])   # 5
+    val2 = deep_get(d, ['x', 'y'])        # 42
+    val3 = deep_get(d, ['a', 'b', 'z'])   # None
+    val4 = deep_get(d, ['a', 'b'])        # {'c': 5}
+    val5 = deep_get(d, ['a', 'b', 'c', 'd']) # None
+    print("val1:", val1)
+    print("val2:", val2)
+    print("val3:", val3)
+    print("val4:", val4)
+    print("val5:", val5)
