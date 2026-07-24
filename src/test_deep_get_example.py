eec7ef3326df3e661ae34a777596a23935583249
rahulkarda <rahulkarda2002@gmail.com>
2026-07-24 11:27:34 +0000

    test: add tiny example for deep_get utility in utils.py

--- src/test_deep_get_example.py
+++ src/test_deep_get_example.py
@@ -1,31 +1,23 @@
 from utils import deep_get
 
-def test_deep_get_basic():
-    d = {'a': {'b': {'c': 5}}, 'x': {'y': 42}}
-    assert deep_get(d, ['a', 'b', 'c']) == 5
-    assert deep_get(d, ['x', 'y']) == 42
-    assert deep_get(d, ['a', 'b', 'z']) is None
-    assert deep_get(d, ['a', 'b']) == {'c': 5}
-    assert deep_get(d, ['a', 'b', 'c', 'd']) is None
-
-def test_deep_get_edge_cases():
-    d2 = {'foo': 3, 'bar': {'baz': {'qux': 7}}}
-    assert deep_get(d2, ['foo']) == 3
-    assert deep_get(d2, ['bar', 'baz', 'qux']) == 7
-    assert deep_get(d2, ['bar', 'baz', 'missing']) is None
-    assert deep_get(d2, ['bar', 'baz']) == {'qux': 7}
-    assert deep_get(d2, ['bar', 'baz', 'qux', 'extra']) is None
-
-def test_deep_get_non_dict_mid():
-    d3 = {'a': {'b': 2}}
-    assert deep_get(d3, ['a', 'b', 'c']) is None
-    assert deep_get(d3, ['a', 'b']) == 2
-
-def run_all():
-    test_deep_get_basic()
-    test_deep_get_edge_cases()
-    test_deep_get_non_dict_mid()
-    print("deep_get utility tests passed.")
+def example_deep_get():
+    d = {
+        'a': {'b': {'c': 5}},
+        'x': {'y': 42},
+        'foo': {'bar': 'baz'},
+    }
+    val1 = deep_get(d, ['a', 'b', 'c'])   # 5
+    val2 = deep_get(d, ['x', 'y'])        # 42
+    val3 = deep_get(d, ['a', 'b', 'z'])   # None
+    val4 = deep_get(d, ['foo', 'bar'])    # 'baz'
+    val5 = deep_get(d, ['a', 'b'])        # {'c': 5}
+    val6 = deep_get(d, ['a', 'b', 'c', 'd']) # None
+    print("val1:", val1)
+    print("val2:", val2)
+    print("val3:", val3)
+    print("val4:", val4)
+    print("val5:", val5)
+    print("val6:", val6)
 
 if __name__ == "__main__":
-    run_all()
+    example_deep_get()
